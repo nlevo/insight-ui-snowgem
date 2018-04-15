b1848825eeba24b8c0b72aad1122e752e2caa0c0
Pit <ceo@snowgem.org>
2018-04-15 23:39:15 +0700

    SNG to XSG

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var testnet = false;
-var netSymbol = testnet ? 'TAZ' : 'SNG';
+var netSymbol = testnet ? 'TAZ' : 'XSG';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
 var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
